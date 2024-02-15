9b8fa56a392f329f8e0517512c00b08ff68615d8
Melvin2306 <melvin.rinkleff@gmx.de>
2024-02-15 21:11:26 +0100

    fixed key

--- src/components/side-menu.tsx
+++ src/components/side-menu.tsx
@@ -1,12 +1,12 @@
-"use client";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
-import { RadioIcon } from "lucide-react";
+'use client';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { RadioIcon } from 'lucide-react';
 
-import { ScrollArea } from "@/components/scroll-area";
-import { Button } from "@/components/ui/button";
-import { useKeyPress } from "@/hooks/useKeyPress";
-import { cn } from "@/lib/utils";
+import { ScrollArea } from '@/components/scroll-area';
+import { Button } from '@/components/ui/button';
+import { useKeyPress } from '@/hooks/useKeyPress';
+import { cn } from '@/lib/utils';
 
 export const SideMenu = ({
   children,
@@ -21,35 +21,35 @@ export const SideMenu = ({
 }) => {
   const router = useRouter();
   useKeyPress(onKeyPress, [
-    "Digit1",
-    "Digit2",
-    "Digit3",
-    "Digit4",
-    "Digit5",
-    "Digit6",
+    'Digit1',
+    'Digit2',
+    'Digit3',
+    'Digit4',
+    'Digit5',
+    'Digit6',
   ]);
 
   function onKeyPress(event: KeyboardEvent) {
     event.preventDefault();
 
-    if (event.code === "Digit1") {
-      router.push("/");
-    } else if (event.code === "Digit2") {
-      router.push("/cv");
-    } else if (event.code === "Digit3") {
-      router.push("/projects");
-    } else if (event.code === "Digit4") {
-      router.push("/stack");
-    } else if (event.code === "Digit6") {
-      router.push("/imprint");
+    if (event.code === 'Digit1') {
+      router.push('/');
+    } else if (event.code === 'Digit2') {
+      router.push('/cv');
+    } else if (event.code === 'Digit3') {
+      router.push('/projects');
+    } else if (event.code === 'Digit4') {
+      router.push('/stack');
+    } else if (event.code === 'Digit5') {
+      router.push('/imprint');
     }
   }
 
   return (
     <ScrollArea
       className={cn(
-        "hidden bg-[var(--background)] lg:bg-[var(--background)] lg:flex lg:flex-col lg:border-r lg:border-b",
-        isInner ? "lg:w-80 xl:w-96" : "lg:w-60 xl:w-72",
+        'hidden bg-[var(--background)] lg:bg-[var(--background)] lg:flex lg:flex-col lg:border-r lg:border-b',
+        isInner ? 'lg:w-80 xl:w-96' : 'lg:w-60 xl:w-72'
       )}
     >
       <div className="p-3">{children}</div>
